3a196c4fef74c9d710e2610ccbe9efe83eeee0f9
Brandon Lim <bdlim2004@gmail.com>
2014-09-05 16:54:57 -0700

    Added other Table Views

---
+++ RideOrganizer/BLGroupsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLGroupsTableViewController.h
+//  RideOrganizer
+//
+//  Created by Brandon Lim on 9/5/14.
+//  Copyright (c) 2014 Brandon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLGroupsTableViewController : UITableViewController
+
+@end
